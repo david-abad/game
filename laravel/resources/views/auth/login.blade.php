ac5c6ea7d0f8252e9e2e38f3cd1dda4ef9190c13
Andrés Villagómez Ríos <tomcar97@gmail.com>
2018-12-08 16:15:51 -0600

    Para mostrar correctamente en movil: login

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -3,6 +3,10 @@
 <head>
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+   <meta charset="utf-8">
+   <meta name="theme-color" content="#212121" />
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         @font-face {
             font-family: 'GoogleFont';
